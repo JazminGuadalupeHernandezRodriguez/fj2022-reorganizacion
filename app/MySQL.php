d7d4a18754188c677b3e85702e5a2fc70788198d
Jazmin Guadalupe Hernandez Rodriguez <19030545@uadeo.mx>
2022-03-31 09:00:30 -0700

    C13

--- app/MySQL.php
+++ app/MySQL.php
@@ -1,3 +1,13 @@
 <?php
-    //TODO: Conexion de MySQL
+    use  \mysqli;
+
+    class MySQL extends \mysqli {
+        protected$data = [
+            'host' => '127.0.0.1',
+            'user' => 'root',
+            'password' => '',
+            'dbname' => 'uadeo',
+            'port' => 3306
+        ];
+    }
 ?>
